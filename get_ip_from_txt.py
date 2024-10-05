313f5af7f8e78b0d343829ce257f576914066cbb
smart2004 <smart2004@inbox.ru>
2024-10-05 15:08:47 +0300

    updated_OCT5th

--- get_ip_from_txt.py
+++ get_ip_from_txt.py
@@ -1,3 +1,11 @@
+import webbrowser
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+# SECRET_KEY = os.getenv('SECRET_KEY')
+api_key = os.getenv('key')
 file = open(r"ids.txt", "r")
 ids = []
 lines = file.readlines()
@@ -9,8 +17,13 @@ for line in lines:
 try:
     for one in ids:
         try:
-            getattr # think on request for geo-API
+            url = f'https://api.ip2location.io/?key={api_key}&ip={one}'
+            webbrowser.open(url)
             count += 1
+
+            url1 = f'https://www.geolocation.com/?ip={one}#ipresult'
+            webbrowser.open(url1)
+        
         except Exception as e:
             print(f"An error occurred for id {one}: {str(e)}")
 except Exception as e:
